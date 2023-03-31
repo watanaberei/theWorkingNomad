4822da12aa888620a5e5f51cd92e1def4ac26b59
watanaberei <to.reiwatanabe@gmail.com>
2023-03-30 20:44:39 -0700

    api 01

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack"); // Add this line
 
+
 module.exports = {
   entry: "./src/index.js",
   output: {
